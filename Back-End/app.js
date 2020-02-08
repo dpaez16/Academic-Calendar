0235405769a3e2e8ecc528d464cbde98525f5772
Danny Paez <dpaez16@yahoo.com>
2020-02-07 18:43:10 -0600

    basic graphql api running

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphQLHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/ac', graphQLHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            users: [String!]!
+        }
+
+        type RootMutation {
+            createUser(name: String, email: String, password: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        users: () => {
+            return ['Danny Paez', 'Alex Paez'];
+        },
+        createUser: (args) => {
+            const { name, email, password } = args;
+            console.log(name);
+            console.log(email);
+            console.log(password);
+
+            return "Done!";
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3000);
