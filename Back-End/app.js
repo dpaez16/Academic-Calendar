7229c7f521166244fd77dc3caa13897e253b1216
Danny Paez <dpaez16@yahoo.com>
2020-03-17 15:16:24 -0500

    DELETE functions remain

--- Back-End/app.js
+++ Back-End/app.js
@@ -10,6 +10,8 @@ const Course = require('./models/course');
 const Category = require('./models/category');
 const CategoryElement = require('./models/categoryElement');
 
+const { getUsers, createUser, editUser } = require('./modules/users');
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -104,77 +106,9 @@ app.use('/ac', graphQLHttp({
         }
     `),
     rootValue: {
-        users: async () => {
-            try {
-                const users = await User.find();
-                return users.map(user => {
-                    return { ...user._doc };
-                });
-            }
-            catch (err) {
-                throw err;
-            }
-        },
-        createUser: async rawArgs => {
-            const args = rawArgs.userInput;
-            return User.findOne({email: args.email}).then(user => {
-                if (user) {
-                    throw new Error('User exists already.');
-                }
-                return bcrypt.hash(args.password, 12);
-            })
-            .then(async hashedPassword => {
-                const newUser = new User({
-                    name: args.name,
-                    email: args.email,
-                    password: hashedPassword,
-                    courses: []
-                });
-                
-                try {
-                    const result = await newUser.save();
-                    return { ...result._doc, password: null };
-                }
-                catch (err) {
-                    throw err;
-                }
-            }).catch(err => {
-                throw err;
-            });
-        },
-        editUser: async rawArgs => {
-            let args = rawArgs.userInput;
-            return bcrypt.hash(args.password, 12).then(async hashedPassword => {
-                return User.findById(rawArgs.userID)
-                .then(async user => {
-                    if (!user) {
-                        throw new Error("Cannot find user.");
-                    }
-
-                    const oldEmail = user.email;
-                    user.name = args.name;
-                    user.email = args.email;
-                    user.password = hashedPassword;
-
-                    if (oldEmail !== args.email) {
-                        return User.findOne({ email: args.email })
-                        .then(foundUser => {
-                            if (foundUser) {
-                                throw new Error("User with that email exists.");
-                            }
-                            return user.save();
-                        });
-                    }
-                    return user.save();
-                })
-                .then(result => {
-                    return { ...result._doc, password: null };
-                });
-            })
-            .catch(err => {
-                throw err;
-            });
-        },
+        users: getUsers,
+        createUser: createUser,
+        editUser: editUser,
         courses: async () => {
             try {
                 const courses = await Course.find();
