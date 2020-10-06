afe0abe79727fed62f9265ff33e6105e8b0e9b73
Danny Paez <dpaez16@yahoo.com>
2020-10-05 20:06:06 -0700

    made navbar an actual component

--- Front-End/src/components/navbar/navbar.js
+++ Front-End/src/components/navbar/navbar.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 import './navbar.css';
 
-export const NavBar = _ => {
-    return (
-        <header className='navbar'>
-            <h1 className='navbar__icon'>
-                <NavLink to='/'>📅</NavLink>
-            </h1>
-            <nav className='navbar__items'>
-                <ul>
-                    <li><NavLink to='/login'>Login</NavLink></li>
-                    <li><NavLink to='/login'>Login</NavLink></li>
-                    <li><NavLink to='/login'>Login</NavLink></li>
-                </ul>
-            </nav>
-        </header>
-    );
+export class NavBar extends Component {
+    render() {
+        return (
+            <header className='navbar'>
+                <h1 className='navbar__icon'>
+                    <NavLink to='/'>📅</NavLink>
+                </h1>
+                <nav className='navbar__items'>
+                    <ul>
+                        <li><NavLink to='/login'>Login</NavLink></li>
+                        <li><NavLink to='/login'>Login</NavLink></li>
+                        <li><NavLink to='/login'>Login</NavLink></li>
+                    </ul>
+                </nav>
+            </header>
+        );
+    }
 }
