6a948ad65ebff9c0e1d822020654984f6d5cbbeb
Danny Paez <dpaez16@yahoo.com>
2020-10-05 21:01:16 -0700

    most of login is done. need to implement login

--- Front-End/src/components/login/login.js
+++ Front-End/src/components/login/login.js
@@ -3,24 +3,64 @@ import {Form, Input, Button, Message} from 'semantic-ui-react';
 import './login.css';
 
 export class Login extends Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            badLogin: false,
+            email: '',
+            password: ''
+        };
+    }
+
+    validInput() {
+        return this.state.email && this.state.password;
+    }
+
+    attemptLogin() {
+        return this.state.email == "d";
+    }
+
     render() {
         return (
-            <Form className='login-form'>
+            <Form   className='login-form'
+                    error={this.state.badLogin}
+            >
                 <Form.Field>
                     <label>E-Mail</label>
-                    <input />
+                    <Input  value={this.state.email}
+                            onChange={e => {
+                                this.setState({email: e.target.value});
+                            }}
+                    />
                 </Form.Field>
                 <Form.Field>
                     <label>Password</label>
-                    <input type='password' />
+                    <Input  type='password'
+                            value={this.state.password}
+                            onChange={e => {
+                                this.setState({password: e.target.value});
+                            }}
+                    />
                 </Form.Field>
-                <Button type='submit'>Login</Button>
-
                 <Message 
                     error
                     header='Login Failed'
                     content='E-Mail or password is incorrect.'
                 />
+                <Button type='submit'
+                        disabled={!this.validInput()}
+                        onClick={async () => {
+                            let loginStatus = this.attemptLogin();
+                            this.setState({
+                                badLogin: !loginStatus,
+                                email: '',
+                                password: ''
+                            });
+                        }}
+                >
+                    Login
+                </Button>
             </Form>
         );
     }
