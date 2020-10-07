708860d8157d6d24231801c08d6aa9e52fa1ba37
Danny Paez <dpaez16@yahoo.com>
2020-10-06 21:27:55 -0700

    fixed courses component

--- Front-End/src/components/courses/courses.js
+++ Front-End/src/components/courses/courses.js
@@ -6,16 +6,67 @@ import './courses.css';
 export class Courses extends Component {
     constructor(props) {
         super(props);
-        console.log(this.props);
+
+        this.state = {
+            courses: []
+        };
+    }
+
+    async getCourses(courseIDS) {
+        const requestBody = {
+            query: `
+            {
+                courses(courseIDS: [${courseIDS}]) {
+                    subject
+                    courseNum
+                    courseName
+                }
+            }
+            `
+        };
+
+        try {
+            const res = await fetch(PROXY_URL, {
+                method: "POST",
+                body: JSON.stringify(requestBody),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            if (res.status !== 200 && res.status !== 201) {
+                throw new Error("Courses fetch failed!");
+            }
+            const resData = await res.json();
+            if (!resData.data.courses) {
+                throw new Error("Courses fetch failed!");
+            }
+
+            const courses = resData.data.courses;
+            return courses;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    componentDidMount() {
+        const courseIDS = this.props.courses.map(course => `"${course._id}"`);
+        this.getCourses(courseIDS)
+        .then(courses => this.setState({courses: courses}));
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.courses !== this.props.courses) {
+            this.setState({courses: this.props.courses});
+        }
     }
 
     render() {
         return (
             <div>
                 <ul>
-                    {this.props.courses.map((course, i) => {
+                    {this.state.courses.map((course, i) => {
                         return (
-                            <li key={i}>{course._id}</li>
+                        <li key={i}>{course.subject}{course.courseNum} - {course.courseName}</li>
                         );
                     })}
                 </ul>
