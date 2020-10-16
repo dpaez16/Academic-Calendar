08c49309317378bf211491286317d28e1516a0fc
Danny Paez <dpaez16@yahoo.com>
2020-10-15 18:43:12 -0700

    centered home screen

--- Front-End/src/components/home/homePage.js
+++ Front-End/src/components/home/homePage.js
@@ -6,12 +6,17 @@ import './homePage.css';
 export const HomePage = _ => {
     return (
         <div className='home-page'>
+            <div>
             <Header size='huge'>
-                Welcome to the Academic-Calendar!
+                Welcome to the Academic Calendar!
             </Header>
             <p>
-                (Description Here)
+                Once you're logged in, enter your course information (assignments, exams, weights, etc.) 
+                and you'll be able to see a beautiful visualization of where you roughly stand in your course!
+                The visualization requires that you enter the rough mean and standard deviation of your course,
+                and it'll calculate your grade after a typical course curve has been applied.
             </p>
+            </div>
         </div>
     );
 }
